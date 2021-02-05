3b63604b6455ce8243253c7c39f99cc8df08dc79
phanitejak <phanitejak1992@users.noreply.github.com>
2021-02-05 19:28:23 +0530

    SON_Blog projet for my organisation

--- SON_Blog/sonblog/config.py
+++
@@ -1,9 +0,0 @@
-
-class Config:
-    SECRET_KEY = 'c425c9dd864d5737847c93f14fed0b0191924ab035886713f8d16f265159fb05'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sonblogdata.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'kondapalliphaniteja@gmail.com'
-    MAIL_PASSWORD = 'PhaniSravaniK@143'
