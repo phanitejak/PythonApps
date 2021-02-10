4b0103f3d51a65239dad957ad70198a23ac674ce
phanitejak <phanitejak1992@users.noreply.github.com>
2021-02-10 16:41:48 +0530

    Update

--- SON_Blog2/sonblog/sonblog/config.py
+++ SON_Blog2/sonblog/sonblog/config.py
@@ -1,10 +1,16 @@
+import os
+
 class Config:
+    mysql_db_host = os.environ.get("MYSQL_DB_HOST", default=True)
     SECRET_KEY = 'c425c9dd864d5737847c93f14fed0b0191924ab035886713f8d16f265159fb05'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///sonblogdata.db'
     # SQLALCHEMY_DATABASE_URI = 'mysql://phani:123456@172.20.10.6/son_blog'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Shravs_143@172.20.10.6/son_blog'
+    if mysql_db_host:
+        SQLALCHEMY_DATABASE_URI = 'mysql://root:Phani_123@' + mysql_db_host.lower() + '/son_blog'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'mysql://phani:123456@sonblogdb/son_blog'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'kondapalliphaniteja@gmail.com'
-    MAIL_PASSWORD = 'PhaniSravaniK@143'
+    MAIL_PASSWORD = ''
